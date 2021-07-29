7567f972d13ca86c4a337a400e684b97c7a25d96
chizzy01 <elvischizzy09@gmail.com>
2021-07-29 19:30:38 +0100

    files are been added to header

--- src/header/Header.js
+++ src/header/Header.js
@@ -5,10 +5,9 @@ const Header = () => {
     return (
         <div className="header">
             <div className="headerTitles">
-                <span className="headerTitleSm">React & Node</span>
-                <span className="headerTitleLg">Blog</span>
+                <span className="headerTitleLg ml-5">Blog App</span>
             </div>
-            <img className="headerImg" src="https://images.pexels.com/photos/4503273/pexels-photo-4503273.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+            <img className="headerImg" src="https://images.pexels.com/photos/4503273/pexels-photo-4503273.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="headerImg" />
         </div>
     )
 }
