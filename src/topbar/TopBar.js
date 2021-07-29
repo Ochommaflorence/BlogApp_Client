5d6c948a0de93465df70e6e77a0e29befd98f559
chizzy01 <elvischizzy09@gmail.com>
2021-07-29 18:39:48 +0100

    adding files to TopBar

--- src/topbar/TopBar.js
+++ src/topbar/TopBar.js
@@ -4,9 +4,22 @@ import "./topBar.css";
 const TopBar = () => {
     return (
         <div className="top">
-            <div className="top-left"></div>
-            <div className="top-left"></div>
-            <div className="top-left"></div>
+            <div className="topLeft">
+            <i class="fab fa-facebook-square"></i> 
+            <i class="fab fa-twitter-square"></i>
+            <i class="fab fa-pinterest-square"></i>
+            <i class="fab fa-instagram-square"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">Home</li>
+                    <li className="topListItem">About</li>
+                    <li className="topListItem">Contact</li>
+                    <li className="topListItem">Write</li>
+                    <li className="topListItem">LogOut</li>
+                </ul>
+            </div>
+            <div className="topRight">r</div>
         </div>
     )
 }
