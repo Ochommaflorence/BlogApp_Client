008069772b98126252afcddf48e587d1e177f787
chizzy01 <elvischizzy09@gmail.com>
2021-07-29 19:17:16 +0100

    files are been added to header

--- src/header/Header.js
+++ src/header/Header.js
@@ -8,6 +8,7 @@ const Header = () => {
                 <span className="headerTitleSm">React & Node</span>
                 <span className="headerTitleLg">Blog</span>
             </div>
+            <img className="headerImg" src="https://images.pexels.com/photos/4503273/pexels-photo-4503273.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
         </div>
     )
 }
