f40845f86ec5369da9a545f623f27851681a26b0
chizzy01 <elvischizzy09@gmail.com>
2021-07-29 19:59:18 +0100

    files are been commited in sideBar

--- src/sideBar/SideBar.js
+++ src/sideBar/SideBar.js
@@ -4,7 +4,14 @@ import './sideBar.css';
 const sideBar = () => {
     return (
         <div className="sideBar">
-            sideBar
+            <div className="sidebarItem">
+                <span className="sidebarTitle">About Me</span>
+                <img src="https://images.pexels.com/photos/859199/pexels-photo-859199.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="sideBarImg" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nostrum quibusdam, autem explicabo est dolore voluptate reprehenderit tempora eos ad nihil natus quaerat nesciunt reiciendis in vero totam distinctio esse.</p>
+            </div>
+            <div className="sidebarItem">
+            <span className="sidebarTitle">Categories</span>
+            </div>
         </div>
     )
 }
