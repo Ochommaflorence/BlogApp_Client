279537b5136786aee72273a19e4412a0f9526b88
chizzy01 <elvischizzy09@gmail.com>
2021-07-29 20:22:34 +0100

    files are been added to sidebar

--- src/sideBar/SideBar.js
+++ src/sideBar/SideBar.js
@@ -6,7 +6,9 @@ const sideBar = () => {
         <div className="sideBar">
             <div className="sidebarItem">
                 <span className="sidebarTitle">About Me</span>
-                <img src="https://images.pexels.com/photos/859199/pexels-photo-859199.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="sideBarImg" />
+                <img 
+                className="sidebarImg"
+                src="https://images.pexels.com/photos/949671/pexels-photo-949671.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="sideBarImg" />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nostrum quibusdam, autem explicabo est dolore voluptate reprehenderit tempora eos ad nihil natus quaerat nesciunt reiciendis in vero totam distinctio esse.</p>
             </div>
             <div className="sidebarItem">
