8d295d70071125ea53099185f63f6534cc13774e
chizzy01 <elvischizzy09@gmail.com>
2021-08-02 14:57:51 +0100

    files are been committed

--- src/post/post.js
+++ src/post/post.js
@@ -3,7 +3,12 @@ import './post.css';
 const post = () => {
   return (
     <div className="post">
-      post
+      <img 
+      className="postImg"
+      src="https://images.pexels.com/photos/5682132/pexels-photo-5682132.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="postImg" />
+      <div className="postInfo">
+        <div className="postCategories"></div>
+      </div>
     </div>
   );
 }
