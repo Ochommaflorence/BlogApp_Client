6c758605854190d07cb65fd92dec285df42c3b9d
chizzy01 <elvischizzy09@gmail.com>
2021-08-02 15:01:18 +0100

    files are committed in postsPage

--- src/post/post.js
+++ src/post/post.js
@@ -7,7 +7,13 @@ const post = () => {
       className="postImg"
       src="https://images.pexels.com/photos/5682132/pexels-photo-5682132.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="postImg" />
       <div className="postInfo">
-        <div className="postCategories"></div>
+        <div className="postCategories">
+          <span className="postCategories">Music</span>
+          <span className="postCategories">Life</span>
+        </div>
+        <span className="postTitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </span>
+        <hr />
+        <span className="postDate"></span>
       </div>
     </div>
   );
