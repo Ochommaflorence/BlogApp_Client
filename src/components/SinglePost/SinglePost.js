fd9987bc224894c24520795e55c6f85dd3717ff5
chizzy01 <elvischizzy09@gmail.com>
2021-08-03 15:10:16 +0100

    committed some files

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -16,7 +16,7 @@ const SinglePost = () => {
               <span className="singlePostAuthor">Author: <b>Florrie</b></span>
               <span className="singlePostDate">1 hour ago</span>
           </div>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          <p className="singlePostDescrition">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           Lorem Ipsum has been the industry's standard dummy text ever since the
           1500s, when an unknown printer took a galley of type and scrambled it to
           make a type specimen book. It has survived not only five centuries, but
