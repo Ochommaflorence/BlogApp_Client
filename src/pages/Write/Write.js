3751bdd0f10a589433a4750f71a5f99bb63912ad
chizzy01 <elvischizzy09@gmail.com>
2021-08-03 15:51:40 +0100

    files are committed

--- src/pages/Write/Write.js
+++ src/pages/Write/Write.js
@@ -9,7 +9,10 @@ const Write = () => {
             <i className="fas fa-plus"></i>
             </label>
             <input type="file" id="fileInput" style={{display: 'none'}} />
-            <input type="text" placeholder="Title" className="writeInput" />
+            <input type="text" placeholder="Title" className="writeInput" autoFocus={true} />
+        </div>
+        <div className="writeFormGroup">
+            <textarea placeholder="Tell your story"></textarea>
         </div>
     </form>
     </div>
