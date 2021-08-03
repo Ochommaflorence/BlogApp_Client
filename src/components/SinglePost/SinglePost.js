b40095c2bff17da7c8ce6b93ee1e9cffb6ef9107
chizzy01 <elvischizzy09@gmail.com>
2021-08-03 14:36:14 +0100

    files are committed

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -3,7 +3,10 @@ import './singlePost.css';
 const SinglePost = () => {
   return (
     <div className="singlePost">
-      single Post
+      <div className="singlePostWrapper">
+          <img src="https://images.pexels.com/photos/4210789/pexels-photo-4210789.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="singlePostImg"/>
+          <h1 className="singlePostTitle"></h1>
+      </div>
     </div>
   );
 }
