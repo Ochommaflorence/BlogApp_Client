264689c21825a7737196c728f04b150624ab18e2
chizzy01 <elvischizzy09@gmail.com>
2021-08-03 14:41:20 +0100

    files are been committed

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -5,7 +5,13 @@ const SinglePost = () => {
     <div className="singlePost">
       <div className="singlePostWrapper">
           <img src="https://images.pexels.com/photos/4210789/pexels-photo-4210789.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="singlePostImg"/>
-          <h1 className="singlePostTitle"></h1>
+          <h1 className="singlePostTitle">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              <div className="singlePostEdit">
+              <i class="far fa-edit"></i>
+              <i class="far fa-trash-alt"></i>
+              </div>
+          </h1>
       </div>
     </div>
   );
