dbec1072820b6f754159e7194d740c5ca2b7dbed
chizzy01 <elvischizzy09@gmail.com>
2021-08-04 16:53:41 +0100

    files are committed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Home from './pages/home/Home';
 import TopBar from './components/topbar/TopBar';
 import Single from './pages/Singlepage/Single';
 import Write from './pages/Write/Write';
+import { Settings } from './pages/Settings/Settings';
 
 function App() {
   return (
    <div>
      <TopBar/>
      {/* <Home/> */}
-     <Write/>
+     {/* <Write/> */}
+     <Settings/>
    </div>
   );
 }
--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import './settings.css'
+import SideBar from '../../components/sideBar/SideBar'
 
 export const Settings = () => {
     return (
         <div className="settings"> 
-            Settings
+            <div className="settingsWrapper"></div>
+            <SideBar/>
         </div>
     )
 }
