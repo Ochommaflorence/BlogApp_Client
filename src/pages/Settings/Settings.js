e0ab6e1e0d5090c3e5224ff881c9846b0548e54b
chizzy01 <elvischizzy09@gmail.com>
2021-08-04 16:57:21 +0100

    files are been committed

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -4,7 +4,15 @@ import SideBar from '../../components/sideBar/SideBar'
 export const Settings = () => {
     return (
         <div className="settings"> 
-            <div className="settingsWrapper"></div>
+            <div className="settingsWrapper">
+                <div className="settingsTitle">
+                    <span className="settingsUpdateTitle">Update Your Account</span>
+                    <span className="settingsDeleteTitle">Delete  Account</span>
+                </div>
+                <form  className="settingsForm">
+                    <label>Profile Picture</label>
+                </form>
+            </div>
             <SideBar/>
         </div>
     )
