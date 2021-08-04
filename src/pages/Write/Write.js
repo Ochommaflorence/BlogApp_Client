cbc88195270425bc19cb6771de34a9ca72f57f77
chizzy01 <elvischizzy09@gmail.com>
2021-08-04 16:38:17 +0100

    files are been added

--- src/pages/Write/Write.js
+++ src/pages/Write/Write.js
@@ -3,6 +3,9 @@ import './write.css';
 const Write = () => {
   return (
     <div className="write">
+        <img 
+        className="writeImg"
+        src="https://images.pexels.com/photos/4321565/pexels-photo-4321565.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="writeImg" />
     <form className="writeForm">
         <div className="writeFormGroup">
             <label htmlFor="fileInput">
