fde0c3ce24c73644db544fd58cba8afbba8c4208
chizzy01 <elvischizzy09@gmail.com>
2021-08-04 17:03:20 +0100

    files are been pushed

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -11,6 +11,12 @@ export const Settings = () => {
                 </div>
                 <form  className="settingsForm">
                     <label>Profile Picture</label>
+                    <div className="settingPP">
+                        <img src="https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500l" alt="" />
+                        <label htmlFor="fileInput">
+                        <i className="far fa-user-circle"></i>
+                        </label>
+                    </div>
                 </form>
             </div>
             <SideBar/>
