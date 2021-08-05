754e9e2d4b9d2f3b2fd76d442722ee545725cd0f
chizzy01 <elvischizzy09@gmail.com>
2021-08-05 20:46:56 +0100

    committing flies

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,9 +6,9 @@ export const Login = () => {
         <span className="loginTitle">Login</span>
       <form className="loginForm">
         <label>Email</label>
-        <input type="text" placeholder="Enter Your Email" />
+        <input type="text" className="loginInput" placeholder="Enter Your Email" />
         <label>Password</label>
-        <input type="password" placeholder="Enter Your Password" />
+        <input type="password" className="loginInput" placeholder="Enter Your Password" />
         <button className="loginButton">Login</button>
       </form>
       <button className="loginRegisterButton">Register</button>
