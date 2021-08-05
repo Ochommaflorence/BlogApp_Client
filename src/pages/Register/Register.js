8925a47ff02ce64e0a403cde825cd189ea73021a
chizzy01 <elvischizzy09@gmail.com>
2021-08-05 20:59:57 +0100

    files are been committed

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,17 @@
+import "./register.css";
+
+export const Register = () => {
+  return (
+    <div className="register">
+        <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label>Email</label>
+        <input type="text" className="registerInput" placeholder="Enter Your Email" />
+        <label>Password</label>
+        <input type="password" className="registerInput" placeholder="Enter Your Password" />
+        <button className="registerButton">Register</button>
+      </form>
+      <button className="registerLoginButton">Login</button>
+    </div>
+  );
+};
