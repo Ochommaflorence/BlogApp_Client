a232c4645d036480efe3cc1d3c03b04dd42588d0
chizzy01 <elvischizzy09@gmail.com>
2021-08-05 20:36:27 +0100

    files are been comitted

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,6 +3,7 @@ import "./login.css";
 export const Login = () => {
   return (
     <div className="login">
+        <span className="loginTitle">Login</span>
       <form className="loginForm">
         <label>Email</label>
         <input type="text" placeholder="Enter Your Email" />
@@ -10,6 +11,7 @@ export const Login = () => {
         <input type="password" placeholder="Enter Your Password" />
         <button className="loginButton">Login</button>
       </form>
+      <button className="loginRegisterButton">Register</button>
     </div>
   );
 };
